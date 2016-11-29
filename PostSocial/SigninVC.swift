90bc3ed2deb0fcea5a45947acb6b0c81ce39c885
PomHaffs <tomashaffenden@icloud.com>
2016-11-29 12:51:26 +1100

    Added Facebook login

--- PostSocial/SigninVC.swift
+++ PostSocial/SigninVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,36 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+//1. Authenticate with FB
+    @IBAction func facebookButtonPressed(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("TOM: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("TOM: User cancelled Facebook authentication")
+            } else {
+                print("TOM: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+//Generic sign in for multiple use
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("TOM: Unable to authenticate with Firebase")
+            } else {
+                print("TOM: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
