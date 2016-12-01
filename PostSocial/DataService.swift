ff2a9341386402d6975e2a62f325ceb444bafd7a
PomHaffs <tomashaffenden@icloud.com>
2016-12-01 11:45:48 +1100

    Added DB structure

--- PostSocial/DataService.swift
+++ PostSocial/DataService.swift
@@ -20,5 +20,48 @@ class DataService {
 //This is singleton statement
     static let ds = DataService()
     
+    private var _REF_BASE = DB_BASE
+    private var _REF_POSTS = DB_BASE.child("Posts")
+    private var _REF_USERS = DB_BASE.child("Users")
+ 
+//GOOD coding for all private var's
+    var REF_BASE: FIRDatabaseReference {
+        return _REF_BASE
+    }
+    var REF_POSTS: FIRDatabaseReference {
+        return _REF_POSTS
+    }
+    var REF_USERS: FIRDatabaseReference {
+        return _REF_USERS
+    }
+
+//This is the set up to the DB
+    func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
+                            //NEW USER SETUP
+//If new data = create, if data already there = update
+        REF_USERS.child(uid).updateChildValues(userData)
+        
+    }
+    
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
